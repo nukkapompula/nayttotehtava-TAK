ccb30fc4b7fdcc25304b6e0a5909af887a0e8f78
Arto Eskelinen <arto.eske@gmail.com>
2024-05-10 13:00:53 +0300

    hahmotelma käyttäjien luomisesta

--- user-creator.sh
+++ user-creator.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 while IFS="," read -r firstName lastName password group
 do
-    echo $firstName
-    echo $lastName
-    echo $password
-    echo $group
-    echo ""
+        echo "$firstName.$lastName"
+        echo $password
+        echo $group
+        echo ""
 done < <(tail -n +2 $1)
+
+# sudo useradd -mg $firstName.$lastName $group
+# sudo chpasswd $firstName.$lastName:$password
