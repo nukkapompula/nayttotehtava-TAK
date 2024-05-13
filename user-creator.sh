65acbb6625d74e47bc1f46dfb55b50079322414f
Arto Eskelinen <arto.eske@gmail.com>
2024-05-13 13:21:04 +0300

    useradd pilkottu osiin

--- user-creator.sh
+++ user-creator.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 function create_html {
-        touch /home/$1.$2/public_html/index.html
-        cat > index.html << EOF
-        <!DOCTYPE html>
-        <html lang="en">
-                <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Index</title>
-                </head>
-                <body>
-                        <p>Tämän sivun omistaa $1 $2.</p>
-                </body>
-        </html>
+touch /home/$1.$2/public_html/index.html
+cat > index.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Index</title>
+        </head>
+        <body>
+                <p>Tämän sivun omistaa käyttäjä <b>$1 $2.</b></p>
+        </body>
+</html>
 EOF
 }
 
 while IFS="," read -r firstName lastName password group
 do
-        sudo useradd -mg $firstName.$lastName $group
+        sudo groupadd testi
+        sudo useradd -m $firstName.$lastName
+        sudo usermod -c "$firstName $lastName $group" $firstName.$lastName
+        sudo usermod -g testi $firstName.$lastName
         sudo chpasswd $firstName.$lastName:$password
-        mkdir /home/$firstName.$lastName/public_html
+        sudo mkdir /home/$firstName.$lastName/public_html
         create_html $firstName $lastName
-        chmod -R ug+x $firstName.$lastName
-        chmod -R o-rwx $firstName.$lastName
+        chmod -R ug+x /home/$firstName.$lastName
+        chmod -R o-rwx /home/$firstName.$lastName
 done < <(tail -n +2 $1)
