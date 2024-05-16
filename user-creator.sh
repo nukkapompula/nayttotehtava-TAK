eeaf7ee182c8bfe05aa033e5b9b7a567bed0f0d9
Arto Eskelinen <arto.eske@gmail.com>
2024-05-16 10:56:47 +0300

    testailua ryhmien kanssa

--- user-creator.sh
+++ user-creator.sh
@@ -16,17 +16,17 @@ cat > ./public_html/index.html << EOF
 EOF
 }
 
-while IFS="," read -r firstName lastName password group
+while IFS="," read firstName lastName password group
 do
-        sudo groupadd testi
+        sudo groupadd $group
         sudo useradd -m $firstName.$lastName
         sudo usermod -c "$firstName $lastName $group" $firstName.$lastName
-        sudo usermod -g testi $firstName.$lastName
-        sudo usermod -a -G testi $USER
+        sudo usermod -g $group $firstName.$lastName
+        sudo usermod -a -G $group $USER
         echo "$firstName.$lastName:$password" | sudo chpasswd
         mkdir public_html
         create_html $firstName $lastName
         sudo mv -t /home/$firstName.$lastName public_html
         sudo chmod -R ug+rwx,o-rwx /home/$firstName.$lastName
-        sudo chgrp -R testi /home/$firstName.$lastName
+        sudo chgrp -R $group /home/$firstName.$lastName
 done < <(tail -n +2 $1)
